ec57407e70f813161637030d1823edf44010b781
nordluma <nordluma@arcada.fi>
2021-02-24 20:17:27 +0200

    removeDuplicates from manufacturers

--- middleware/Utils.js
+++ middleware/Utils.js
@@ -2,6 +2,7 @@ const fetch = require("node-fetch");
 
 const BASE_URL = "https://bad-api-assignment.reaktor.com/v2/";
 
+// Implement if time
 exports.fetchData = async (category, req, res, next) => {
     try {
         const response = await fetch(`${BASE_URL}/products/${category}`);
@@ -13,24 +14,33 @@ exports.fetchData = async (category, req, res, next) => {
     }
 };
 
-exports.getAvailability = async (category, req, res, next) => {
-    const availability_url =
-        "https://bad-api-assignment.reaktor.com/v2/availability/";
+exports.removeDuplicates = async (data) => {
     try {
-        let avalilabilityData = await Promise.all(
-            category.map((p) =>
-                fetch(availability_url + p.manufacturer).then((res) =>
-                    res.json()
-                )
-            )
-        );
-        return avalilabilityData;
+        const manufacturers = await data.map((p) => p.manufacturer);
+        const uniqueManufacturers = [...new Set(manufacturers)];
+
+        return uniqueManufacturers;
     } catch (err) {
         this.errorHandling;
     }
 };
 
-exports.errorHandling = (err, req, res, next) => {
+exports.getAvailability = async (manufacturer, req, res, next) => {
+    const availability_url =
+        "https://bad-api-assignment.reaktor.com/v2/availability/";
+    try {
+        const request = await fetch(availability_url + manufacturer);
+        const data = await request.json();
+
+        console.log(data);
+
+        return data;
+    } catch (err) {
+        this.errorHandling;
+    }
+};
+
+exports.mergeData = exports.errorHandling = (err, req, res, next) => {
     const error = new Error(err);
     error.status = err.status || 500;
     next(error);
