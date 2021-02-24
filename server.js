0e1030df546f548ac43a7dbf1179d31ef9375557
nordluma <nordluma@arcada.fi>
2021-02-24 11:42:05 +0200

    setting up server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bp = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+// Middleware
+app.use(morgan("dev"));
+app.use(bp.urlencoded({ extended: false }));
+app.use(bp.json());
+
+app.use("/products", prdouctRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error("Requested resource not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res) => {
+    res.status(error.status || 500).json({
+        status: error.status,
+        error: error.message,
+    });
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
